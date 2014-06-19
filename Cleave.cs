64232e2a7f514fee4097c8de3f241a947d831e4b
dranelol <cardsfan853@gmail.com>
2014-06-18 21:03:06 -0700

    initial commit

---
+++ Cleave.cs
@@ -0,0 +1,272 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cleave : Ability
+{
+    public Cleave(AttackType attackType, DamageType damageType, float range, float angle, float cooldown, float damageMod, float resourceCost, string id, string readable, GameObject particles)
+        : base(attackType, damageType, range, angle, cooldown, damageMod, resourceCost, id, readable, particles, 0)
+    {
+       
+    }
+
+    // PBAoE AttackHandler
+    public override void AttackHandler(GameObject source, Entity attacker, bool isPlayer)
+    {
+        List<GameObject> attacked = OnAttack(source, isPlayer);
+
+        if (isPlayer == true)
+        {
+            // this is player -> enemy
+            foreach (GameObject enemy in attacked)
+            {
+                // if our target isn't dead or resetting
+                if (enemy.GetComponent<AIController>().IsResetting() == false
+                    && enemy.GetComponent<AIController>().IsDead() == false)
+                {
+                    // get defender, do damage, put the target in combat if they weren't before
+                    Entity defender = enemy.GetComponent<Entity>();
+                    DoDamage(source, enemy, attacker, defender, isPlayer);
+                    GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().RunCoroutine(DoHitAnimation(source, GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().OnHitNormalParticles, 0.2f, isPlayer, defender.gameObject));
+                    // GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().RunCoroutine(DoAnimation(defender.gameObject, GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().OnHitNormalParticles, 0.2f, isPlayer, defender.gameObject));
+
+                    if (enemy.GetComponent<AIController>().IsInCombat() == false)
+                    {
+                        enemy.GetComponent<AIController>().BeenAttacked(source);
+                    }
+                    if(attacker.abilityManager.abilities[6] != null)
+                    {
+                        attacker.abilityManager.abilities[6].AttackHandler(attacker.gameObject, defender.gameObject, isPlayer);
+                    }
+                }
+            }
+        }
+
+        else
+        {
+            // this is enemy -> player
+            foreach (GameObject enemy in attacked)
+            {
+                Entity defender = enemy.GetComponent<Entity>();
+                DoDamage(source, enemy, attacker, defender, isPlayer);
+                if (attacker.abilityManager.abilities[6] != null)
+                {
+                    attacker.abilityManager.abilities[6].AttackHandler(attacker.gameObject, defender.gameObject, isPlayer);
+                }
+                
+            }
+        }
+
+        GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().RunCoroutine(DoAnimation(source, particleSystem, 0.2f, isPlayer));
+    }
+
+    public override List<GameObject> OnAttack(GameObject source, bool isPlayer)
+    {
+        List<GameObject> enemiesToAttack = new List<GameObject>();
+
+        Vector3 forward = new Vector3();
+
+        // if this is a player attack, forward attack vector will be based on cursor position
+        if (isPlayer == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit target;
+            Physics.Raycast(ray, out target, Mathf.Infinity);
+            Vector3 vectorToMouse = target.point - source.transform.position;
+            forward = new Vector3(vectorToMouse.x, source.transform.forward.y, vectorToMouse.z).normalized;
+        }
+
+
+        int enemyMask = LayerMask.NameToLayer("Enemy");
+        int playerMask = LayerMask.NameToLayer("Player");
+
+        Collider[] colliders;
+
+        // if this is a player attack, check for enemies in range
+        if (isPlayer == true)
+        {
+
+            colliders = Physics.OverlapSphere(source.transform.position, range + source.GetComponent<MovementFSM>().Radius, 1 << enemyMask);
+
+        }
+
+         // if this is an enemy attack, check for players in range
+        else
+        {
+            colliders = Physics.OverlapSphere(source.transform.position, range + source.GetComponent<MovementFSM>().Radius, 1 << playerMask);
+
+        }
+
+        foreach (Collider collider in colliders)
+        {
+            
+
+            // create a vector from the possible enemy to the attacker
+            Vector3 normalizedAttackPosition = new Vector3(source.transform.position.x, 1, source.transform.position.z);
+            Vector3 normalizedDefenderPosition = new Vector3(collider.transform.position.x, 1, collider.transform.position.z);
+
+            Vector3 enemyVector = normalizedDefenderPosition - normalizedAttackPosition;
+            Vector3 enemyVector2 = normalizedAttackPosition - normalizedDefenderPosition;
+
+            // this is an enemy attack, forward attack vector will be based on target position
+            if (isPlayer == false)
+            {
+                forward = enemyVector;
+            }
+
+            // if the angle between the forward vector of the attacker and the enemy vector is less than the angle of attack, the enemy is within the attack angle
+            if (Vector3.Angle(forward, enemyVector) < angle)
+            {
+                RaycastHit hit = new RaycastHit();
+
+                if (isPlayer == true)
+                {
+                    // try to cast a ray from the enemy to the player
+
+                    //bool rayCastHit = Physics.Raycast(new Ray(normalizedDefenderPosition, enemyVector2), out hit, range + source.GetComponent<MovementFSM>().Radius, ~(1 << enemyMask));
+
+                    bool rayCastHit = CombatMath.RayCast(source.transform, collider.transform, out hit, range, ~(1 << enemyMask));
+                    Debug.DrawRay(normalizedDefenderPosition, enemyVector, Color.green, 0.5f);
+                    Debug.DrawRay(normalizedDefenderPosition, enemyVector2, Color.red, 0.5f);
+                    if (!rayCastHit)
+                    {
+
+                    }
+                    // if the ray hits, the enemy is in line of sight of the player, this is a successful attack hit
+                    else
+                    {
+                        if (hit.collider.gameObject.tag == "Player")
+                        {
+
+                            Debug.DrawRay(normalizedDefenderPosition, enemyVector, Color.green, 0.5f);
+                            Debug.DrawRay(normalizedDefenderPosition, enemyVector2, Color.red, 0.5f);
+
+                            enemiesToAttack.Add(collider.gameObject);
+                        }
+                    }
+                }
+
+                else
+                {
+                    // try to cast a ray from the player to the enemy
+
+                    //bool rayCastHit = Physics.Raycast(new Ray(normalizedDefenderPosition, enemyVector2), out hit, range + source.GetComponent<MovementFSM>().Radius, ~(1 << playerMask));
+
+                    bool rayCastHit = CombatMath.RayCast(source.transform, collider.transform, out hit, range, ~(1 << playerMask));
+
+                    if (!rayCastHit)
+                    {
+                    }
+                    // if the ray hits, the player is in line of sight of the enemy, this is a successful attack hit
+                    else
+                    {
+                        if (hit.collider.gameObject.tag == "Enemy")
+                        {
+                            //Debug.DrawRay(normalizedDefenderPosition, enemyVector, Color.green, 0.5f);
+                            //Debug.DrawRay(normalizedDefenderPosition, enemyVector2, Color.red, 0.5f);
+
+                            enemiesToAttack.Add(collider.gameObject);
+                        }
+
+                        else
+                        {
+                            Debug.Log("not something that we didn't hit");
+                            Debug.Log(hit.collider.name);
+                        }
+                    }
+                }
+            }
+        }
+
+        return enemiesToAttack;
+    }
+
+
+    public override void DoDamage(GameObject source, GameObject target, Entity attacker, Entity defender, bool isPlayer)
+    {
+
+        // run damage calc, apply damage
+        float damageAmt;
+        if (isPlayer == true)
+        {
+            damageAmt = DamageCalc.DamageCalculation(attacker, defender, damageMod);
+        }
+        else
+        {
+            damageAmt = DamageCalc.DamageCalculation(attacker, defender, 0);
+        }
+
+        defender.ModifyHealth(-damageAmt);
+
+    }
+
+    public IEnumerator DoHitAnimation(GameObject source, GameObject particlePrefab, float time, bool isPlayer, GameObject target)
+    {
+        // instantiate particle system for the on-hit of this ability
+        GameObject particles;
+
+        particles = (GameObject)GameObject.Instantiate(particlePrefab, CombatMath.GetCenter(target.transform), target.transform.rotation);
+        // wait for "time" seconds
+        yield return new WaitForSeconds(time);
+        // get all particle systems from the prefab
+        ParticleSystem[] particleSystems = particles.GetComponentsInChildren<ParticleSystem>();
+
+        // unparent each particle system, and disable emission. we do this to make sure that each particle system has a chance to finish its full cycle before being destroyed
+        // each particle system has a script to destroy itself when emission fully stops
+        foreach (ParticleSystem item in particleSystems)
+        {
+            item.transform.parent = null;
+            item.emissionRate = 0;
+            item.enableEmission = false;
+
+        }
+
+        // destroy the entire particle system object
+        GameObject.Destroy(particles);
+
+        yield return null;
+    }
+
+    public override IEnumerator DoAnimation(GameObject source, GameObject particlePrefab, float time, bool isPlayer, GameObject target = null)
+    {
+        GameObject particles;
+        // instantiate particle system for the on-hit of this ability
+        // if the player is casting the ability, we need to activate it based on the position of the cursor, not the transform's forward
+        if (isPlayer == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit targetRay;
+            Physics.Raycast(ray, out targetRay, Mathf.Infinity);
+            Vector3 vectorToMouse = targetRay.point - source.transform.position;
+            Vector3 cursorForward = new Vector3(vectorToMouse.x, source.transform.forward.y, vectorToMouse.z).normalized;
+
+
+            Quaternion rotation = Quaternion.LookRotation(cursorForward);
+            particles = (GameObject)GameObject.Instantiate(particlePrefab, source.transform.position, rotation);
+        }
+
+        else
+        {
+            particles = (GameObject)GameObject.Instantiate(particlePrefab, source.transform.position + source.transform.forward * source.GetComponent<MovementFSM>().Radius * 0.5f, source.transform.rotation);
+
+        }
+        // wait for "time" seconds
+        yield return new WaitForSeconds(time);
+        // get all particle systems from the prefab
+        ParticleSystem[] particleSystems = particles.GetComponentsInChildren<ParticleSystem>();
+        // unparent each particle system, and disable emission. we do this to make sure that each particle system has a chance to finish its full cycle before being destroyed
+        // each particle system has a script to destroy itself when emission fully stops
+        foreach (ParticleSystem item in particleSystems)
+        {
+            item.transform.parent = null;
+            item.emissionRate = 0;
+            item.enableEmission = false;
+
+        }
+        // destroy the entire particle system object
+        GameObject.Destroy(particles);
+
+        yield return null;
+    }
+
+}
---
+++ ProjectileBehaviour.cs
@@ -0,0 +1,365 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProjectileBehaviour : MonoBehaviour 
+{
+    /// <summary>
+    /// The creator of this projectile
+    /// </summary>
+    public GameObject owner;
+
+    /// <summary>
+    /// Velocity of the rigidbody 
+    /// </summary>
+    public Vector3 velocity;
+
+    /// <summary>
+    /// The public identifier of the ability tied to this projectile on hit
+    /// </summary>
+    public string abilityID;
+
+    /// <summary>
+    /// Constant of acceleration to apply to the projectile. For a consistant speed, this is default set to 0.0f
+    /// </summary>
+    public float accelerationConstant = 0.0f;
+
+    /// <summary>
+    /// This is a maximum time that the projectile can remain alive before activating
+    /// </summary>
+    public float timeToActivate;
+
+    /// <summary>
+    /// Whether or not this projectile exists until interacted with
+    /// </summary>
+    public bool Infinite = false;
+
+    /// <summary>
+    /// this sees if the projectile has collided with anything yet. Keeps it from colliding with two things
+    /// </summary>
+    public bool hasCollided = false;
+
+    /// <summary>
+    /// flag for if the projectile collides with other projectiles instead of players/enemies
+    /// </summary>
+    public bool CollidesWithProjectiles = false;
+
+    /// <summary>
+    /// flag for if the projectile(homing only right now) cares about terrain 
+    /// </summary>
+    public bool CollidesWithTerrain = true;
+
+    /// <summary>
+    /// For projectiles that have an AoE attack on explode
+    /// </summary>
+    public bool AOEOnExplode = false;
+
+    /// <summary>
+    /// flag for if the projectile does it's attack when it times out
+    /// </summary>
+    public bool ExplodesOnTimeout = false;
+
+    /// <summary>
+    /// flag for if the projectile kills itself when it hits a target
+    /// </summary>
+    public bool DiesOnHit = true;
+
+    /// <summary>
+    /// flag for if the projectile kills itself when it hits its owner
+    /// </summary>
+    public bool DiesOnOwnerHit = false;
+
+    /// <summary>
+    /// counter for... things. use it for whatever.
+    /// </summary>
+    public int count;
+
+    /// <summary>
+    /// A list of every player and enemy the projectile has collided with.
+    /// </summary>
+    public List<GameObject> HasCollidedWith;
+
+
+
+    /// <summary>
+    /// If this projectile needs a target, this is it
+    /// </summary>
+    public Vector3 target = Vector3.zero;
+
+    /// <summary>
+    /// if the projectile needs to home to an object, this is it
+    /// </summary>
+    public GameObject targetObject = null;
+
+    /// <summary>
+    /// Is this a homing projectile?
+    /// </summary>
+    public bool homing = false;
+
+    /// <summary>
+    /// Speed of the projectile
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// If true, then this projectile isn't attributed to any ability
+    /// </summary>
+    public bool EnvironmentProjectile = false;
+
+    void Awake()
+    {
+    }
+
+	void Start () 
+    {
+	    
+	}
+
+	void Update () 
+    {
+        timeToActivate -= Time.deltaTime;
+
+        //Debug.Log(timeToActivate);
+
+        //velocity = rigidbody.velocity;
+
+        // if this is a homing projectile, rotate towards our homing target
+
+        if (homing == true)
+        {
+            // if there is a target object, use its transform as our target position
+            if (targetObject != null)
+            {
+                Vector3 direction = targetObject.transform.position - transform.position;
+
+                Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, 0.3f, 0f);
+                transform.rotation = Quaternion.LookRotation(newDirection);
+            }
+
+            // else, use "target" as target position
+            else
+            {
+
+                Vector3 direction = target - transform.position;
+
+                Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, 0.3f, 0f);
+                transform.rotation = Quaternion.LookRotation(newDirection);
+            }
+
+            
+        }
+
+        // move along the path
+        transform.position = transform.position + transform.forward * Time.deltaTime * speed;
+        speed += accelerationConstant * Time.deltaTime;
+
+        // explode time, if applicable
+        if (timeToActivate <= 0.0f)
+        {
+            // if we need to explode
+            if (ExplodesOnTimeout == true)
+            {
+
+                if (owner.gameObject.tag == "Player")
+                {
+
+                    // carry out explosion ability
+                    if (EnvironmentProjectile == false)
+                    {
+                        Entity ownerEntity = owner.GetComponent<Entity>();
+
+                        int abilityIndex = ownerEntity.abilityIndexDict[abilityID];
+
+                        ownerEntity.abilityManager.abilities[abilityIndex].AttackHandler(gameObject, owner, ownerEntity, true);
+                    }
+                    hasCollided = true;
+                    DetachParticleSystem();
+                    Destroy(gameObject);
+
+
+                }
+
+                else if (owner.gameObject.tag == "Enemy")
+                {
+
+                    // carry out explosion ability
+                    if (EnvironmentProjectile == false)
+                    {
+                        Entity ownerEntity = owner.GetComponent<Entity>();
+
+                        int abilityIndex = ownerEntity.abilityIndexDict[abilityID];
+
+                        ownerEntity.abilityManager.abilities[abilityIndex].AttackHandler(gameObject, owner, ownerEntity, false);
+                    }
+                    hasCollided = true;
+                    DetachParticleSystem();
+                    Destroy(gameObject);
+                }
+            }
+
+            else
+            {
+                DetachParticleSystem();
+                Destroy(gameObject);
+            }
+        }
+
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (hasCollided == false)
+        {
+            // if we havent collided with, and we collide with projectiles, check if the trigger we just entered was another projectile
+
+            if (CollidesWithProjectiles == true)
+            {
+                if (other.gameObject.tag == "Projectile")
+                {
+                    if (other.gameObject.GetComponent<ProjectileBehaviour>().owner.tag == "Player")
+                    {
+                        // carry out projectile explosion ability
+                      
+
+                        if (EnvironmentProjectile == false)
+                        {
+                            Entity ownerEntity = owner.GetComponent<Entity>();
+
+                            int abilityIndex = ownerEntity.abilityIndexDict[abilityID];
+
+                            ownerEntity.abilityManager.abilities[abilityIndex].AttackHandler(owner, other.gameObject, owner.GetComponent<Entity>(), true);
+                        }
+                        hasCollided = true;
+                        DetachParticleSystem();
+                        Destroy(gameObject);
+                    }
+
+                }
+            }
+
+            // else, normal explosion
+
+            else if (EnvironmentProjectile == true && other.gameObject.tag == "Terrain")
+            {
+                Debug.Log("environment hit!");
+                if (DiesOnHit == true)
+                {
+                    hasCollided = true;
+                    DetachParticleSystem();
+                    Destroy(gameObject);
+                }
+                
+            }
+
+            else if (CollidesWithTerrain == true && other.gameObject.tag == "Terrain")
+            {
+                // if this is a pbaoe attack, do the attack handler at point of collision
+                if (AOEOnExplode == true)
+                {
+                    Entity ownerEntity = owner.GetComponent<Entity>();
+                    int abilityIndex = ownerEntity.abilityIndexDict[abilityID];
+                    ownerEntity.abilityManager.abilities[abilityIndex].AttackHandler(gameObject, other.gameObject, owner.GetComponent<Entity>(), true);
+
+                }
+
+                if (DiesOnHit == true)
+                {
+                    hasCollided = true;
+                    DetachParticleSystem();
+                    Destroy(gameObject);
+                }
+            }
+
+            else
+            {
+                Debug.Log(other.gameObject.tag + " and " + owner.gameObject.tag);
+
+                // normal collision between enemy and player
+                if (other.gameObject.tag == "Enemy" && owner.gameObject.tag == "Player")
+                {
+                    if (HasCollidedWith.Contains(other.gameObject) == false)
+                    {
+                        Debug.Log("attacked an enemy: " + gameObject.name.ToString());
+                        HasCollidedWith.Add(other.gameObject);
+
+                        // if this isn't an environment projectile; carry out normal attack handler
+                        if (EnvironmentProjectile == false)
+                        {
+                            Entity ownerEntity = owner.GetComponent<Entity>();
+
+                            int abilityIndex = ownerEntity.abilityIndexDict[abilityID];
+
+                            ownerEntity.abilityManager.abilities[abilityIndex].AttackHandler(owner, other.gameObject, owner.GetComponent<Entity>(), true);
+                        }
+
+                        // if the projectile should die on hit, detach its particle system and destroy
+                        if (DiesOnHit == true)
+                        {
+
+                            hasCollided = true;
+                            DetachParticleSystem();
+                            Destroy(gameObject);
+                        }
+                    }
+
+                }
+                // normal collision between player and enemy
+                else if (other.gameObject.tag == "Player" && owner.gameObject.tag == "Enemy")
+                {
+                    if (HasCollidedWith.Contains(other.gameObject) == false)
+                    {
+                        Debug.Log("attacked a player");
+                        HasCollidedWith.Add(other.gameObject);
+
+                        // if this isn't an environment projectile; carry out normal attack handler
+                        if (EnvironmentProjectile == false)
+                        {
+                            Entity ownerEntity = owner.GetComponent<Entity>();
+
+                            int abilityIndex = ownerEntity.abilityIndexDict[abilityID];
+
+                            ownerEntity.abilityManager.abilities[abilityIndex].AttackHandler(owner, other.gameObject, owner.GetComponent<Entity>(), false);
+                        }
+                        // if the projectile should die on hit, detach its particle system and destroy
+                        if (DiesOnHit == true)
+                        {
+
+                            hasCollided = true;
+                            DetachParticleSystem();
+                            Destroy(gameObject);
+                        }
+                    }
+                }
+
+                
+
+                
+                if (other.gameObject == owner.gameObject && DiesOnOwnerHit == true)
+                {
+                    Debug.Log("hit owner");
+                    hasCollided = true;
+                    DetachParticleSystem();
+                    Destroy(gameObject);
+                }
+            }
+
+            
+        }
+    }
+
+    public void DetachParticleSystem()
+    {
+        // get all particle systems from this object
+        ParticleSystem[] particles = GetComponentsInChildren<ParticleSystem>();
+        // unparent each particle system, and disable emission. we do this to make sure that each particle system has a chance to finish its full cycle before being destroyed
+        // each particle system has a script to destroy itself when emission fully stops
+        foreach (ParticleSystem item in particles)
+        {
+            item.transform.parent = null;
+            item.emissionRate = 0;
+            //item.enableEmission = false; 
+        }
+        //particles.GetComponent<ParticleAnimator>().autodestruct = true;
+    }
+}
